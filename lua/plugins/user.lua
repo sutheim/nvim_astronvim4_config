5a26edbd517591f11bebf5bbfd3a2240954d4c83
sutheim <sondreutheim@gmail.com>
2024-06-07 09:15:08 +0200

    added nu support

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -4,4 +4,9 @@ return {
     "jvgrootveld/telescope-zoxide",
     config = function() require("telescope").load_extension "zoxide" end,
   },
+  {
+    "LhKipp/nvim-nu",
+    lazy = false,
+    config = function() require'nu'.setup{} end,
+  }
 }
