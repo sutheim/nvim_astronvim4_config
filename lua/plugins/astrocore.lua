32ca892e891c15438373585d0598245f5ee15185
Sondre <sondreutheim@gmail.com>
2024-06-21 15:58:47 +0200

    added bqf

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -57,8 +57,8 @@ return {
         ["N"] = { "Nzz", desc = "Next and center" },
         ["n"] = { "nzz", desc = "Next and center" },
 
-        ["<C-k>"] = { "yyP", desc = "Duplicate line up" },
-        ["<C-j>"] = { "yyp", desc = "Duplicate line down" },
+        ["<C-K>"] = { "yyP", desc = "Duplicate line up" },
+        ["<C-J>"] = { "yyp", desc = "Duplicate line down" },
 
         ["<C-u>"] = { "<C-u>zz", desc = "Half page up and center" },
         ["<C-d>"] = { "<C-d>zz", desc = "Half page down and center" },
--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -8,5 +8,10 @@ return {
     "LhKipp/nvim-nu",
     lazy = false,
     config = function() require'nu'.setup{} end,
+  },
+  {
+    "kevinhwang91/nvim-bqf",
+    ft = "qf",
+    opts = {},
   }
 }
